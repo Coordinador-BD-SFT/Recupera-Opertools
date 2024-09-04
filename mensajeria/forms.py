c0e42ad2fabd269d06e1c2bea6d7056987b09345
Coordinador Operativo <coordinador.basesdedatos@gmail.com>
2024-09-04 08:50:08 -0500

    Documentando formularios y URLs en app Mensajeria

--- mensajeria/forms.py
+++ mensajeria/forms.py
@@ -2,13 +2,21 @@ from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Field, Layout
 
+# Create your forms here
+
+# Formulario para el envío auomatico de whatsapp
+
 
 class WhatsappScrapingForm(forms.Form):
     messages = forms.FileField(required=True, label='Mensajes')
 
+    # Reescribimos el constructor para utilizar libreria de renderizado de formularios con Bootstrap5
     def __init__(self, *args, **kwargs):
+        # Llamamos al constructor padre para crear correctamente la instancia del formulario
         super().__init__(*args, **kwargs)
+        # Definimos variables para el renderizado con Bootstrap5
         self.helper = FormHelper()
         self.helper.layout = Layout(
+            # Agregamos clases e identificadores Bootstrap5
             Field('messages', css_class='form-control', id='validationCustom01')
         )
--- mensajeria/urls.py
+++ mensajeria/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+# (Convención) Definimos variable app_name para referenciar las urls mas fácilmente
 app_name = 'mensajeria'
+
+# (Convención) Lista de funciones path que definen las URLs del proyecto
 urlpatterns = [
     path('', views.index, name='index'),
     path('whatsapp_scraping', views.whatsapp_scraping, name='whatsapp_scraping'),
